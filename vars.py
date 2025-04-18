40d2368b2955f9e205a5d5e20c9514c0c62fcf82
smurfndakyu <bhumisingh22006@gmail.com>
2025-04-18 16:39:57 +0530

    Delete vars.py

--- vars.py
+++
@@ -1,7 +0,0 @@
-api_id = 25552615 #change_with_your_Api_id.
-api_hash = "b2ae2b696d592e1752304fe8521d0c7f" #change_with_your_Api_hash.
-bot_token = "7743757492:AAGxsrgTsrZzsmFb8Zv87tr93Fd9LSzSztI" #change_with_your_bot_token.
-auth_users = [6154078289] #change_with_your_user_id.
-sudo_user = 6154078289 #change_with_your_user_id.
-log_channel = -1002404836332 #change_with_your_channel_id.
-txt_channel = -1002404836332 #change_with_your_channel_id.
