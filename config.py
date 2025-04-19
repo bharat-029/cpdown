ebb57b7a4830f0f6d5f6c2e1353494c9637cd88f
smurfndakyu <bhumisingh22006@gmail.com>
2025-04-19 18:37:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "18524985"
 api_hash = "df8841a3752a0dea57924d2991693aa9"
 bot_token = "7893850397:AAEnqae2GCiTxj5HG1n5ZqSlCtIXhmg99xg"
-auth_users = [7523934472]
+auth_users = [7523934472 8172163893]
 
