2f112d6c6372939fa4ad7e2a18f9855be7ec4d72
bharat-029 <bharatbis9oi29@gmail.com>
2025-04-24 10:53:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "18524985"
-api_hash = "df8841a3752a0dea57924d2991693aa9"
-bot_token = "7893850397:AAEnqae2GCiTxj5HG1n5ZqSlCtIXhmg99xg"
-auth_users = [7523934472 8172163893]
+api_id = "23664819"
+api_hash = "3853f97c662d5d08cee5f0d07361361e"
+bot_token = "7963498040:AAF_XBo4LIv0JzDXJvSeX_pqN5Qp4yMme5o"
+auth_users = [8004315740,7744345481]
 
